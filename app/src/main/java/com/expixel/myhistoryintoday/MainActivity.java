f2cd6af9d32f6581da369d2a1e07ad3b81a5ee7e
AustinTing <austintingwork@gmail.com>
2016-11-04 13:44:28 +0800

    Update colors.xml

--- app/src/main/java/com/expixel/myhistoryintoday/MainActivity.java
+++ app/src/main/java/com/expixel/myhistoryintoday/MainActivity.java
@@ -1,18 +1,27 @@
 package com.expixel.myhistoryintoday;
 
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import static com.expixel.myhistoryintoday.R.drawable.calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
     @Override
@@ -49,4 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
 }
