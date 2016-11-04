75ab445bfeba8a0d0afbcae23779b9c0dec4bea5
CellBody <austintingwork@gmail.com>
2016-11-04 09:42:02 +0800

    Add beworker/pinned-section-listview

--- app/src/main/java/com/expixel/myhistoryintoday/MainActivity.java
+++ app/src/main/java/com/expixel/myhistoryintoday/MainActivity.java
@@ -1,5 +1,7 @@
 package com.expixel.myhistoryintoday;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+import de.halfbit.pinnedsection.PinnedSectionListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ListAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initializeHeaderAndFooter();
+        initializeAdapter();
+        initializePadding();
     }
 
     @Override
@@ -48,5 +65,96 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    static class SimpleAdapter extends ArrayAdapter<Item> implements PinnedSectionListView.PinnedSectionListAdapter {
+
+        private static final int[] COLORS = new int[] {
+                R.color.green_light, R.color.orange_light,
+                R.color.blue_light, R.color.red_light };
+
+        public SimpleAdapter(Context context, int resource, int textViewResourceId) {
+            super(context, resource, textViewResourceId);
+            generateDataset('A', 'Z', false);
+        }
+
+        public void generateDataset(char from, char to, boolean clear) {
+
+            if (clear) clear();
+
+            final int sectionsNumber = to - from + 1;
+            prepareSections(sectionsNumber);
+
+            int sectionPosition = 0, listPosition = 0;
+            for (char i=0; i<sectionsNumber; i++) {
+                Item section = new Item(Item.SECTION, String.valueOf((char)('A' + i)));
+                section.sectionPosition = sectionPosition;
+                section.listPosition = listPosition++;
+                onSectionAdded(section, sectionPosition);
+                add(section);
+
+                final int itemsNumber = (int) Math.abs((Math.cos(2f*Math.PI/3f * sectionsNumber / (i+1f)) * 25f));
+                for (int j=0;j<itemsNumber;j++) {
+                    Item item = new Item(Item.ITEM, section.text.toUpperCase(Locale.ENGLISH) + " - " + j);
+                    item.sectionPosition = sectionPosition;
+                    item.listPosition = listPosition++;
+                    add(item);
+                }
+
+                sectionPosition++;
+            }
+        }
+
+        protected void prepareSections(int sectionsNumber) { }
+        protected void onSectionAdded(Item section, int sectionPosition) { }
+
+        @Override public View getView(int position, View convertView, ViewGroup parent) {
+            TextView view = (TextView) super.getView(position, convertView, parent);
+            view.setTextColor(Color.DKGRAY);
+            view.setTag("" + position);
+            Item item = getItem(position);
+            if (item.type == Item.SECTION) {
+                //view.setOnClickListener(PinnedSectionListActivity.this);
+                view.setBackgroundColor(parent.getResources().getColor(COLORS[item.sectionPosition % COLORS.length]));
+            }
+            return view;
+        }
+
+        @Override public int getViewTypeCount() {
+            return 2;
+        }
+
+        @Override public int getItemViewType(int position) {
+            return getItem(position).type;
+        }
+
+        @Override
+        public boolean isItemViewTypePinned(int viewType) {
+            return viewType == Item.SECTION;
+        }
+
+    }
+
+    static class Item {
+
+        public static final int ITEM = 0;
+        public static final int SECTION = 1;
+
+        public final int type;
+        public final String text;
+
+        public int sectionPosition;
+        public int listPosition;
+
+        public Item(int type, String text) {
+            this.type = type;
+            this.text = text;
+        }
+
+        @Override
+        public String toString() {
+            return text;
+        }
+
     }
 }
