fad8df6bd18f337e0a2e007d8c00b2960186916d
CellBody <austintingwork@gmail.com>
2022-04-09 23:25:43 +0700

    feat: change login page

--- pages/login.js
+++ pages/login.js
@@ -10,7 +10,26 @@ export default function Login () {
         <link rel='icon' href='/favicon.svg' />
       </Head>
 
-      <main className={styles.main} />
+      <main className={styles.main}>
+        <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between'>
+          <h2 className='text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
+            <span className='block'>On this day</span>
+            <span className='block text-pink-500'>Meet Your History</span>
+          </h2>
+          <div className='mt-8 flex lg:mt-0 lg:flex-shrink-0'>
+            <div className='inline-flex rounded-md shadow'>
+              <a
+                href='#'
+                className='inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-pink-500 hover:bg-pink-700'
+              >
+                Login
+              </a>
+            </div>
+
+          </div>
+        </div>
+      </main>
+
       <footer className={styles.footer}>
         <a
           href='https://github.com/AustinTing/my-history-today'
