7fd87e1dbd4ea72009452dbf8c2d226e4f65eed3
CellBody <austintingwork@gmail.com>
2022-04-11 23:58:47 +0700

    feat: test swr

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { SWRConfig } from 'swr'
 import '../styles/globals.css'
 
 function MyHistoryToday ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        onError: (err) => {
+          console.error(err)
+        }
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
 
 export default MyHistoryToday
