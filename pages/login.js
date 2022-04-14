434c85bf3f6b8b09dde0c6f1c8dd6d99634ff02a
CellBody <austintingwork@gmail.com>
2022-04-15 00:49:31 +0700

    improvement: login page css

--- pages/login.js
+++ pages/login.js
@@ -14,21 +14,11 @@ export default function Login () {
 
   return (
     <Layout>
-      <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between'>
+      <div className='flex-col max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center '>
         <h2 className='text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
           <span className='block'>On this day</span>
           <span className='block text-pink-500'>Meet Your History</span>
         </h2>
-        {/* <div className='mt-8 flex lg:mt-0 lg:flex-shrink-0'>
-            <div className='inline-flex rounded-md shadow'>
-              <a
-                href='#'
-                className='inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-pink-500 hover:bg-pink-700'
-              >
-                Login
-              </a>
-            </div>
-          </div> */}
         <div className='login'>
           <Form
             errorMessage={errorMessage}
