1368a869b6da8ff0bf93f83745409889dc295cfc
CellBody <austintingwork@gmail.com>
2022-04-21 00:23:29 +0700

    refactor: Heart icon

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,12 @@
 // import styled from 'styled-components'
-// import Heart from 'components/icons/Heart'
+import Heart from 'components/icons/Heart'
 
 const Footer = () => {
   return (
     <footer className='flex flex-1 p-8 border-t-2 border-gray-50 justify-start items-end'>
       <a href='https://github.com/AustinTing/my-history-today' className='flex justify-center items-center grow'>
         Made with
-        <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5 ml-1 mr-1 text-rose-600 fill-current' viewBox='0 0 20 20'>
-          <path
-            fillRule='evenodd'
-            d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'
-            clipRule='evenodd'
-          />
-        </svg>
+        <Heart className='h-5 w-5 ml-1 mr-1 text-rose-600 fill-current' viewBox='0 0 20 20' />
         by Austin Ting
       </a>
     </footer>
--- components/icons/Heart.js
+++ components/icons/Heart.js
@@ -1,8 +1,8 @@
 import Svg from './Svg'
 
-export default function Heart ({ className }) {
+export default function Heart ({ className, viewBox }) {
   return (
-    <Svg viewBox='0 0 20 20' className={className}>
+    <Svg viewBox={viewBox} className={className}>
       <path
         fillRule='evenodd'
         d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'
