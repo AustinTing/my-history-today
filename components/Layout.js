c3a54f07d82e429f9f5c0c95ff5cf826439e22c0
CellBody <austintingwork@gmail.com>
2022-04-21 23:58:56 +0700

    style: clean code

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,6 @@
 import Header from './Header'
 import Footer from './Footer'
 
-// const Container = styled.div`
-// padding: 0 2rem;
-// `
-// const Main = styled.div`
-// min-height: 100vh;
-// padding: 4rem 0;
-// flex: 1;
-// display: flex;
-// flex-direction: column;
-// justify-content: center;
-// align-items: center;
-// `
-
 const Layout = ({ children }) => {
   return (
     <container className='p-8'>
