6dd6da5278c6b09b199a94f8a086b57a4c3e2351
CellBody <austintingwork@gmail.com>
2022-04-21 23:52:36 +0700

    refactor: Heart icon

--- components/icons/Heart.js
+++ components/icons/Heart.js
@@ -1,13 +1,20 @@
-import Svg from './Svg'
 
-export default function Heart ({ className, viewBox }) {
+const Heart = ({ className, viewBox }) => {
   return (
-    <Svg viewBox={viewBox} className={className}>
+    <svg
+      version='1.1'
+      xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+      className={className}
+      viewBox={viewBox}
+    >
       <path
         fillRule='evenodd'
         d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'
         clipRule='evenodd'
       />
-    </Svg>
+    </svg>
   )
 }
+
+export default Heart
