6e0572623a81246cc4ba605da6fb7dc3cc1d028d
CellBody <austintingwork@gmail.com>
2022-04-22 23:46:17 +0700

    fix: footer style

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import Heart from 'components/icons/Heart'
 
 const Footer = () => {
   return (
-    <footer className='flex flex-1 p-8 border-t-2 border-gray-50 justify-start items-end'>
+    <footer className='flex flex-1 p-8 border-t-2 border-gray-50 justify-center items-center'>
       <a href='https://github.com/AustinTing/my-history-today' className='flex justify-center items-center grow'>
         Made with
         <Heart className='h-5 w-5 ml-1 mr-1 text-rose-600 fill-current' viewBox='0 0 20 20' />
