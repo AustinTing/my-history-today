8c1f21e3a5fa9110b46d02cc77f2693362bb645f
CellBody <austintingwork@gmail.com>
2022-04-26 23:41:24 +0700

    feat: add googl callback

--- pages/api/auth/google/callback.js
+++ pages/api/auth/google/callback.js
@@ -3,6 +3,7 @@ export default function handler (req, res) {
   if (method === 'POST') {
     // Process a POST request
     console.log(`POST /google/callback, body: ${body}`)
+    return res.status(200).send()
   }
   res.status(400).send()
 }
